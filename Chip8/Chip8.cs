2091b41ad62689d57dd4f1eab9ac056fdb2a41e9
Circlebit <paul.seibel@gmail.com>
2019-02-14 21:14:15 +0100

    First Steps, SDL experiments

---
+++ Chip8/Chip8.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8
+{
+    class Chip8
+    {
+        #region Registers
+        // General Purpose Registers (8 Bit)
+
+        /// <summary>
+        /// General Purpose Register 0 (8 Bit)
+        /// </summary>
+        public byte V0 { get; set; }
+        /// <summary>
+        /// General Purpose Register 1 (8 Bit)
+        /// </summary>
+        public byte V1 { get; set; }
+        /// <summary>
+        /// General Purpose Register 2 (8 Bit)
+        /// </summary>
+        public byte V2 { get; set; }
+        /// <summary>
+        /// General Purpose Register 3 (8 Bit)
+        /// </summary>
+        public byte V3 { get; set; }
+        /// <summary>
+        /// General Purpose Register 4 (8 Bit)
+        /// </summary>
+        public byte V4 { get; set; }
+        /// <summary>
+        /// General Purpose Register 5 (8 Bit)
+        /// </summary>
+        public byte V5 { get; set; }
+        /// <summary>
+        /// General Purpose Register 6 (8 Bit)
+        /// </summary>
+        public byte V6 { get; set; }
+        /// <summary>
+        /// General Purpose Register 7 (8 Bit)
+        /// </summary>
+        public byte V7 { get; set; }
+        /// <summary>
+        /// General Purpose Register 8 (8 Bit)
+        /// </summary>
+        public byte V8 { get; set; }
+        /// <summary>
+        /// General Purpose Register 9 (8 Bit)
+        /// </summary>
+        public byte V9 { get; set; }
+        /// <summary>
+        /// General Purpose Register 10 (8 Bit)
+        /// </summary>
+        public byte VA { get; set; }
+        /// <summary>
+        /// General Purpose Register 11 (8 Bit)
+        /// </summary>
+        public byte VB { get; set; }
+        /// <summary>
+        /// General Purpose Register 12 (8 Bit)
+        /// </summary>
+        public byte VC { get; set; }
+        /// <summary>
+        /// General Purpose Register 13 (8 Bit)
+        /// </summary>
+        public byte VD { get; set; }
+        /// <summary>
+        /// General Purpose Register 14 (8 Bit)
+        /// </summary>
+        public byte VE { get; set; }
+        /// <summary>
+        /// General Purpose Register 15 (8 Bit).
+        /// Should not be used by programs as is used by some instructions
+        /// </summary>
+        public byte VF { get; set; }
+        /// <summary>
+        /// 16 Bit register
+        /// </summary>
+        public ushort VI { get; set; } // 16 Bit register
+
+        /// <summary>
+        /// Program Counter Register (16 Bit)
+        /// </summary>
+        public ushort PC { get; private set; }
+        /// <summary>
+        /// Stack Pointer Register (8 Bit)
+        /// </summary>
+        public byte SP { get; private set; }
+        
+        /// <summary>
+        /// Stack (16 x 16 Bytes)
+        /// </summary>
+        public ushort[] Stack { get; set; } //TODO: Stack Class
+        #endregion
+
+        /// <summary>
+        /// 4K of Memory
+        /// </summary>
+        public byte[] RAM { get; set; }
+
+        public Chip8()
+        {
+            Stack = new ushort[16];
+            RAM = new byte[0x1000];
+        }
+    }
+}
---
+++ Chip8/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using SDL2;
+
+namespace Chip8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myChip8 = new Chip8();
+
+            SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+
+            IntPtr window = SDL.SDL_CreateWindow("Fenstertitel",
+                SDL.SDL_WINDOWPOS_CENTERED,
+                SDL.SDL_WINDOWPOS_CENTERED,
+                640,
+                320,
+                SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
+
+            IntPtr renderer = SDL.SDL_CreateRenderer(window, -1, 0);
+
+            SDL.SDL_RenderSetScale(renderer, 10.0f, 10.0f); // Render a pixels 10 times bigger than they actually actually are
+
+            SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL.SDL_RenderClear(renderer);
+            SDL.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            for (int x = 0; x < 64; x++)
+            {
+                SDL.SDL_RenderDrawPoint(renderer, x, x / 2);
+            }
+            SDL.SDL_RenderPresent(renderer);
+
+
+            // "Game Loop"
+            SDL.SDL_Event e;
+            bool quit = false;
+            while (!quit)
+            {
+                while (SDL.SDL_PollEvent(out e) != 0)
+                {
+                    switch (e.type)
+                    {
+                        case SDL.SDL_EventType.SDL_QUIT:
+                            quit = true;
+                            break;
+                        case SDL.SDL_EventType.SDL_KEYDOWN:
+                            switch (e.key.keysym.sym)
+                            {
+                                case SDL.SDL_Keycode.SDLK_q:
+                                    quit = true;
+                                    break;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            SDL.SDL_DestroyRenderer(renderer);
+            SDL.SDL_DestroyWindow(window);
+            SDL.SDL_Quit();
+        }
+    }
+}
